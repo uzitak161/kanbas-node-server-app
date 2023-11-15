67d9fcc79800e66c5659ba4b55b90a980e948579
Uzay Takil <uzaytakil01@gmail.com>
2023-11-15 14:27:57 -0500

    API calls for fetching classes and posting new class

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from './Lab5.js'
 import cors from "cors";
+import CourseRoutes from "./courses/routes.js"
 
 const app = express()
 app.use(cors());
 app.use(express.json());
+
+
+CourseRoutes(app)
 Hello(app)
 Lab5(app)
 app.listen(4000)
