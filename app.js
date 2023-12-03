6bc92a0847d7653971e634ff29eb8fffa40b233c
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-12-03 13:50:36 -0500

    Deployed

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ import ModuleRoutes from "./modules/routes.js"
 import AssignmentRoutes from "./assignments/routes.js"
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL,
     })
 );
 
@@ -25,6 +28,17 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
+
 app.use(
     session(sessionOptions)
 );
